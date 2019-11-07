44ebe4c3bc4c0cbe649ff0ec73da24b971953a40
Michael Krog <michaelryankrog@gmail.com>
2019-11-06 18:35:21 -0600

    Updates

---
+++ precache-manifest.6f2499ff44566b9a23881511ae3c2d74.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "449dcb6b2ed674a2a7ba07686acc61a9",
+    "url": "/googleBooks/index.html"
+  },
+  {
+    "revision": "044582fed5691f42d58b",
+    "url": "/googleBooks/static/css/main.1df5cc94.chunk.css"
+  },
+  {
+    "revision": "8a80a5396b9c1ec23f9a",
+    "url": "/googleBooks/static/js/2.7be12c47.chunk.js"
+  },
+  {
+    "revision": "044582fed5691f42d58b",
+    "url": "/googleBooks/static/js/main.812a2077.chunk.js"
+  },
+  {
+    "revision": "da32313dcab2b7568fa6",
+    "url": "/googleBooks/static/js/runtime-main.34b51635.js"
+  },
+  {
+    "revision": "ac66c3e997026019909d58455a174233",
+    "url": "/googleBooks/static/media/booksBG.ac66c3e9.jpg"
+  },
+  {
+    "revision": "cd2fd5e73ac34e7f60a68ac6e75e7537",
+    "url": "/googleBooks/static/media/logo.cd2fd5e7.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -11,13 +11,19 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/MRKrog/googleBooks/precache-manifest.8fa18437b70ac863ec8841229105cbae.js"
+  "/googleBooks/precache-manifest.6f2499ff44566b9a23881511ae3c2d74.js"
 );
 
-workbox.clientsClaim();
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
 
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
@@ -25,10 +31,9 @@ workbox.clientsClaim();
  * See https://goo.gl/S9QRab
  */
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
-workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/MRKrog/googleBooks/index.html", {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/googleBooks/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
